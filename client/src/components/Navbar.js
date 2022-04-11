57cc39b43767be8fb05895547ec3f857dc0ae8c5
Leonnardo Nascimento <92021464+N-Leonnardo@users.noreply.github.com>
2022-04-11 15:19:12 -0700

    commit

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -3,15 +3,32 @@ import '../styles/Navbar.css'
 
 const Main = () => {
 
+    const onClickHandler = e => {
+        const alert = document.getElementById("alert-navbar")
+        alert.remove()
+    }
+
 
     return(
-        <div className='navbar'>
-            <img src='https://i.imgur.com/E2lce3U.png' />
-            <div className="links">
-                <a>Home</a>
-                <a>About Leo</a>
+        <div>
+            <div className='navbar'>
+                <img src='https://i.imgur.com/E2lce3U.png' />
+                <div className="links">
+                    <a>Home</a>
+                    <a>About Leo</a>
+                </div>
             </div>
-            
+            <div>
+                <div className="alert-navbar" id="alert-navbar" onClick={onClickHandler}>
+                    <p>
+                        This is a website that you can scan a barcode and add it to our database. Please, dont mess it up or you will be banned.
+                    </p>
+
+                    <p>
+                        x
+                    </p>
+                </div>
+            </div>
         </div>
     )
 }
